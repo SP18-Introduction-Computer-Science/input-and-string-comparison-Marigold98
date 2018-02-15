0d9165e13a4c07c72e8d83afd7d00f98363fd74a
marygolden@lewisu.edu <marygolden@lewisu.edu>
2018-02-15 14:47:51 -0600

    input string and comparison

---
+++ assignment3.py
@@ -0,0 +1,8 @@
+a = ["even instances"]
+b = ["odd instances"]
+for index, line in enumerate(open("strings.txt")) :
+    evenorodd = index % 2
+    if (a) or (b) == 0 :
+    print("strings.txt" + str(a))
+    else :
+    print("string.txt" + str(b))
